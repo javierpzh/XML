82f7d9fddf3536e56ab19d17bfd6bff930f16ef3
Javier Pérez Hidalgo <reyole111@gmail.com>
2020-03-03 19:03:57 +0100

    Función 1

--- ListaPaises/funciones_listadepaises.py
+++ ListaPaises/funciones_listadepaises.py
@@ -1,1 +1,11 @@
-.
+from lxml import etree
+doc=etree.parse("listadepaises.xml")
+lista1=doc.xpath("//continentes/continente/@nombre")
+lista2=doc.xpath("//continente/pais/text()")
+lista3=doc.xpath("//paises/pais/idioma/text()")
+lista4=doc.xpath("//paises/pais/moneda/text()")
+
+def listar_informacion(lista1,lista2):
+    for continente in lista1:
+        cont=doc.xpath("//continentes/continente/@nombre")
+        return cont
