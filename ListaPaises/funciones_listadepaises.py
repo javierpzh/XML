3366e173335d3b7f850cbc8ca6ae1c16d731874e
Javier Pérez Hidalgo <61273236+javierpzh@users.noreply.github.com>
2020-05-06 18:50:54 +0200

    Add files via upload
    final

--- ListaPaises/funciones_listadepaises.py
+++ ListaPaises/funciones_listadepaises.py
@@ -1,39 +1,59 @@
-from lxml import etree
-
+#EJERCICIO 1
 def listar_informacion(doc):
-    lista1=doc.xpath("//continentes/continente/@nombre")
-    for continente in lista1:
+    lista_continentes=doc.xpath("//continentes/continente/@nombre")
+    for elem in lista_continentes:
         cont=doc.xpath("//continentes/continente/@nombre")
     return cont
 
+#EJERCICIO 2
 def contar_informacion(doc):
-    lista2=doc.xpath("//continente/pais/text()")
-    return len(lista2)
+    lista_paises=doc.xpath("//continente/pais/text()")
+    return len(lista_paises)
 
-def buscar_informacion(doc,cadena):
-    lista2=doc.xpath("//continente/pais/text()")
-    for pais in lista2:
-        if cadena in lista2:
-            sispol=doc.xpath('/geografia/paises/pais[@nombre="%s"]/sistema/text()' %(cadena))
-            idioma=doc.xpath('/geografia/paises/pais[@nombre="%s"]/idioma/text()' %(cadena))
+#EJERCICIO 3
+def buscar_informacion(doc,pais):
+    lista_paises=doc.xpath("//continente/pais/text()")
+    for elem in lista_paises:
+        if pais in lista_paises:
+            sispol=doc.xpath('/geografia/paises/pais[@nombre="%s"]/sistema/text()' %(pais))
+            idioma=doc.xpath('/geografia/paises/pais[@nombre="%s"]/idioma/text()' %(pais))
         return sispol,idioma
 
-def buscar_informacion_relacionada(doc,cadena):
-    lista3=doc.xpath("//paises/pais/idioma/text()")
-    for idioma in lista3:
-        if cadena in lista3:
-            pais=doc.xpath('/geografia/paises/pais[idioma="%s"]/idioma/../@nombre' %(cadena))
+#EJERCICIO 4
+def buscar_informacion_relacionada(doc,idioma):
+    lista_idiomas=doc.xpath("//paises/pais/idioma/text()")
+    for elem in lista_idiomas:
+        if idioma in lista_idiomas:
+            pais=doc.xpath('/geografia/paises/pais[idioma="%s"]/idioma/../@nombre' %(idioma))
         return pais
 
-def buscar_moneda(doc,cadena):
-    lista4=doc.xpath("//paises/pais/moneda/text()")
-    lista=[]
-    for moneda in lista4:
-        if cadena in lista4:
-            pais=doc.xpath('/geografia/paises/pais[moneda="%s"]/moneda/../@nombre' %(cadena))
-            lista.append(pais)
-            superficie=doc.xpath('/geografia/paises/pais[moneda="%s"]/moneda/../superficie/text()' %(cadena))
-            lista.append(superficie)
-            idioma=doc.xpath('/geografia/paises/pais[moneda="%s"]/moneda/../idioma/text()' %(cadena))
-            lista.append(idioma)
-        return lista
+#EJERCICIO 5
+def buscar_moneda(doc,moneda):
+    lista_monedas=doc.xpath("//paises/pais/moneda/text()")
+    lista_paises=[]
+    lista_superficies=[]
+    lista_idiomas=[]
+    for elem in lista_monedas:
+        if moneda in lista_monedas:
+            pais=doc.xpath('/geografia/paises/pais[moneda="%s"]/moneda/../@nombre' %(moneda))
+            lista_paises.append(pais)
+            superficie=doc.xpath('/geografia/paises/pais[moneda="%s"]/moneda/../superficie/text()' %(moneda))
+            lista_superficies.append(superficie)
+            idioma=doc.xpath('/geografia/paises/pais[moneda="%s"]/moneda/../idioma/text()' %(moneda))
+            lista_idiomas.append(idioma)
+        return lista_paises,lista_superficies,lista_idiomas
+
+
+#VALIDACIONES
+
+def validar_pais(doc,pais):
+    if pais in doc.xpath("//continente/pais/text()"):
+        return True
+
+def validar_idioma(doc,idioma):
+    if idioma in doc.xpath("//paises/pais/idioma/text()"):
+        return True
+
+def validar_moneda(doc,moneda):
+    if moneda in doc.xpath("//paises/pais/moneda/text()"):
+        return True
--- ListaPaises/listadepaises.py
+++ ListaPaises/listadepaises.py
@@ -1,10 +1,12 @@
+import time
+from lxml import etree
 from funciones_listadepaises import *
 
 doc=etree.parse("listadepaises.xml")
 
 while True:
     menu=('''OPCIONES:
-    1- LISTAR INFORMACIÓN: Mostrar el nombre de los continentes de los que tenemos información, y sus respectivos países.
+    1- LISTAR INFORMACIÓN: Mostrar el nombre de los continentes de los que tenemos información.
     2- CONTAR INFORMACIÓN: Mostrar la cantidad de países de los que tenemos información.
     3- BUSCAR INFORMACIÓN: Pedir por teclado un país y mostrar el tipo de sistema político que tiene y su idioma.
     4- BUSCAR INFORMACIÓN RELACIONADA: Pedir por teclado un idioma y mostrar los países en los que se habla.
@@ -19,32 +21,94 @@ while True:
 
     #EJERCICIO 1
     if opcion==1:
-        for pais in listar_informacion(doc):
-            print(pais)
+        print("Has seleccionado la opción 1:")
+        print("LISTAR INFORMACIÓN: Mostrar el nombre de los continentes de los que tenemos información.")
+        time.sleep(1)
+        print()
+        print("CONTINENTES")
+        for elem in listar_informacion(doc):
+            print(elem)
+        time.sleep(1)
+        print()
+        print()
+        print()
+        print()
 
     #EJERCICIO 2
-    if opcion==2:
+    elif opcion==2:
+        print("Has seleccionado la opción 2:")
+        print("CONTAR INFORMACIÓN: Mostrar la cantidad de países de los que tenemos información.")
+        time.sleep(1)
+        print()
+        print("Nº DE PAÍSES")
         print(contar_informacion(doc))
+        time.sleep(1)
+        print()
+        print()
+        print()
+        print()
 
     #EJERCICIO 3
-    if opcion==3:
-        cadena=str(input("Introduce un país: "))
-        for pais in buscar_informacion(doc,cadena):
-            print(pais)
+    elif opcion==3:
+        print("Has seleccionado la opción 3:")
+        print("BUSCAR INFORMACIÓN: Pedir por teclado un país y mostrar el tipo de sistema político que tiene y su idioma.")
+        time.sleep(1)
+        print()
+        pais=str(input("Introduce un país: "))
+        while validar_pais(doc,pais)!=True:
+            pais=input("No existe este país. Vuelva a intentarlo: ")
+        sispol,idioma=buscar_informacion(doc,pais)
+        print()
+        print("Sistema político: ",sispol[0],"")
+        print("Idioma: ",idioma[0],"")
+        time.sleep(1)
+        print()
+        print()
+        print()
+        print()
 
     #EJERCICIO 4
-    if opcion==4:
-        cadena=str(input("Introduce un idioma: "))
-        for idioma in buscar_informacion_relacionada(doc,cadena):
-            print(idioma)
+    elif opcion==4:
+        print("Has seleccionado la opción 4:")
+        print("BUSCAR INFORMACIÓN RELACIONADA: Pedir por teclado un idioma y mostrar los países en los que se habla.")
+        time.sleep(1)
+        print()
+        idioma=str(input("Introduce un idioma: "))
+        while validar_idioma(doc,idioma)!=True:
+            idioma=input("No existe este idioma. Vuelva a intentarlo: ")
+        print()
+        print("PAÍSES")
+        for elem in buscar_informacion_relacionada(doc,idioma):
+            print(elem)
+        time.sleep(1)
+        print()
+        print()
+        print()
+        print()
 
     #EJERCICIO 5
-    if opcion==5:
-        cadena=str(input("Introduce una moneda: "))
-        for moneda in buscar_moneda(doc,cadena):
-            print(moneda)
+    elif opcion==5:
+        print("Has seleccionado la opción 5:")
+        print("ACERCA DE LA MONEDA: Pedir por teclado una moneda, y mostrar los países en los que se utiliza. Además, que muestre su superficie(km²) y su idioma.")
+        time.sleep(1)
+        print()
+        moneda=str(input("Introduce una moneda: "))
+        while validar_moneda(doc,moneda)!=True:
+            moneda=input("No existe esta moneda. Vuelva a intentarlo: ")
+        lista_paises,lista_superficies,lista_idiomas=buscar_moneda(doc,moneda)
+        for p,s,i in zip(lista_paises,lista_superficies,lista_idiomas):
+            for p,s,i in zip(p,s,i):
+                print(" ",p,"-",s,"km²","-",i)
+        time.sleep(1)
+        print()
+        print()
+        print()
+        print()
 
     #Salir
-    if opcion==6:
+    elif opcion==6:
+        print("Has seleccionado la opción 6:")
+        print("SALIR")
+        time.sleep(1)
         print("FIN DEL PROGRAMA") 
         break  
